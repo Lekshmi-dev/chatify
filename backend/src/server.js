dd74e05c0a73fc5fb081c6c348c1e2f02c11aceb
lekshmiAlwafaa <lekshmi@alwafaagroup.ae>
2026-03-18 12:00:10 +0400

    db setup and signup endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,24 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import { connectDB } from './lib/db.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+if(process.env.NODE_ENV === "produtcion"){
+  app.use(express.static(Path.join(__dirname,"../frontend/dist")));
+  app.get("*",(_,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend/","dist","index.html"));
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
