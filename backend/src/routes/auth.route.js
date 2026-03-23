fa9bc972cce0fb16d0ae088db5d0f18083d2c03d
lekshmi-dev <lekshmi.reachme@gmail.com>
2026-03-23 09:22:56 +0400

    added rate limiting using arcjet

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,10 +2,11 @@ import express from 'express';
 import { signup,login,logout,updateProfile} from '../controllers/auth.controller.js';
 import { protectedRoute } from '../middleware/auth.middleware.js';
 import  upload  from "../middleware/uploadFile.middleware.js";
-const router = express.Router();
+import { arcjetProtection } from '../middleware/arcject.middleware.js';
 
+const router = express.Router();
+router.use(arcjetProtection);
 router.post('/signup', signup);
-router.post('/login',login);
 router.post('/logout',logout); 
 //check authenticated or not->use middleware function 
 router.put('/update-profile',protectedRoute,upload.single("profilePicture"),updateProfile); 
